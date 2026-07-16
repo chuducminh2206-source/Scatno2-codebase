0a8f1f02f379b738475b31a52900432d9c905b3a
RoofusRoof19 <roofusroof19@gmail.com>
2026-07-16 19:35:26 +1200

    Implemented blue button movement

--- main.ino
+++ main.ino
@@ -1,17 +1,23 @@
 int buttonA;
 int buttonAPrev;
+int buttonB;
+int buttonBPrev;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Serial communications started");
 
   buttonA = digitalRead(4);
+  buttonB = digitalRead(5);
   buttonAPrev = buttonA;
+  buttonBPrev = buttonB;
 }
 
 void loop() {
   buttonA = digitalRead(4);
+  buttonB = digitalRead(5);
 
+  /* Red button controls */
   if(buttonAPrev == 0 && buttonA == 1) {
     Serial.println("Button A pressed");
   }
@@ -19,6 +25,17 @@ void loop() {
   if(buttonAPrev == 1 && buttonA == 0) {
     Serial.println("Button A released");
   }
+  
+  /* Blue button controls */
+  if(buttonBPrev == 0 && buttonB == 1) {
+    Serial.println("Button B pressed");
+  }
+  
+  if(buttonBPrev == 1 && buttonB == 0) {
+    Serial.println("Button B released");
+  }
+
 
   buttonAPrev = buttonA;
+  buttonBPrev = buttonB;
 }
